158598068b17d87bab394bf0c3f6c9e23f310ff4
Theam-Dev <phearakroun@gmail.com>
2023-05-21 14:23:45 +0700

    custom Gallery

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
 import './App.css';
+import './Gallery.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import GalleryModal from './components/GalleryModal'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const imgUrls = [
+    'img/1.jpg',
+    'img/2.jpg',
+    'img/3.jpg',
+    'img/4.jpg',
+    'img/6.jpg',
+    'img/7.jpg',
+    'img/8.jpg',
+    'img/9.jpg',
+    'img/10.jpg',
+    'img/11.jpg'
+];
+ class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { currentIndex: null };
+    this.closeModal = this.closeModal.bind(this);
+    this.findNext = this.findNext.bind(this);
+    this.findPrev = this.findPrev.bind(this);
+    this.renderImageContent = this.renderImageContent.bind(this);
+  }
+  renderImageContent(src, index) {
+    return (
+      <div onClick={(e) => this.openModal(e, index)}>
+        <img src={src} key={src} />
+      </div>
+    ) 
+  }
+  openModal(e, index) {
+    this.setState ({ currentIndex: index });
+  }
+  closeModal(e) {
+    if (e != undefined) {
+      e.preventDefault();
+    }
+    this.setState ({ currentIndex: null });
+  }
+  findPrev(e) {
+    if (e != undefined) {
+      e.preventDefault();
+    }
+    this.setState(prevState => ({
+      currentIndex: prevState.currentIndex -1
+    }));
+  }
+  findNext(e) {
+    if (e != undefined) {
+      e.preventDefault();
+    }
+    this.setState(prevState => ({
+      currentIndex: prevState.currentIndex + 1
+    }));
+  }
+  render() {
+    return (
+      <div className="react-gallery">
+        <div className="gallery-grid">
+          {imgUrls.map(this.renderImageContent)}
+        </div>
+        <GalleryModal 
+          closeModal={this.closeModal} 
+          findPrev={this.findPrev} 
+          findNext={this.findNext} 
+          hasPrev={this.state.currentIndex > 0} 
+          hasNext={this.state.currentIndex + 1 < imgUrls.length} 
+          src={imgUrls[this.state.currentIndex]} 
+        />
+      </div>
+    )
+  }
 }
 
 export default App;
+ReactDOM.render(<App />, document.querySelector('.react-gallery'));
